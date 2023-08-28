06c8ebb9c3a8e58297b935f92196cb100762d0f7
Oyebode Adetunji <adetunji.oyebode6@gmail.com>
2023-08-28 12:55:08 +0100

    Create a log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,12 +13,12 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Ffilter values in incoming log records using filter_datum """
+        """ Filter values in incoming log records using filter_datum """
         msg = super(RedactingFormatter, self).format(record)
         txt = filter_datum(self.fields,
                            self.REDACTION,
